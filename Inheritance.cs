c27b8d4b32378a166903a0bb891000156a49ff72
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2022-07-24 15:07:25 +0530

    method hiding & parent method call

---
+++ Inheritance.cs
@@ -0,0 +1,21 @@
+﻿
+namespace BindingApp
+{
+    public class Parent
+    {
+
+        public virtual  void message()
+        {
+            Console.WriteLine("I am Parent Class");
+        }
+    }
+    public class Child:Parent
+    {
+
+        public override  void message()
+        {
+          base.message();
+            Console.WriteLine("I am Child Class");
+        }
+    }
+}
