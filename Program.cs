2c506c7d45554cfe3c69e19333edb01dee9bd0f2
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2022-07-24 15:07:10 +0530

    dynamic type ex

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BindingApp;
+
+
+// Calling  via Parent Ref
+
+Console.WriteLine("Calling via Parent Ref");
+Parent parent = new Child();
+parent .message();
+
+
+// Calling  via child Ref
+
+Console.WriteLine("Calling via Child Ref");
+Child child = new Child();
+child.message();
+
+
+
+// Dynamic Datatype Ex
+
+DynamicEx d = new DynamicEx();
+d.dynmaicType();  
